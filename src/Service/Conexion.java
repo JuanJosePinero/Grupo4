7b5fc9f5dc903e8ce3919057c737e11f2a3d201b
pperez03 <pperez1987149@gmail.com>
2023-05-12 13:42:45 +0200

    Añadida libreria y cambio Conexion

--- src/Service/Conexion.java
+++ src/Service/Conexion.java
@@ -1,5 +1,6 @@
 package Service;
 
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -11,15 +12,21 @@ public class Conexion {
     public static Connection obtener() throws SQLException, ClassNotFoundException {
         if (cnx == null) {
             try {
+                // Verificar la versión del controlador JDBC de MySQL que estás utilizando
+                // Si estás usando MySQL 8.0 o superior
                 Class.forName("com.mysql.cj.jdbc.Driver");
-                cnx = DriverManager.getConnection("jdbc:mysql://localhost/gestionra8?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "root");
+
+                // Si estás usando una versión anterior a MySQL 8.0
+                // Class.forName("com.mysql.jdbc.Driver");
+
+                cnx = DriverManager.getConnection("jdbc:mysql://localhost:3306/gestionra8?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "root");
             } catch (SQLException ex) {
                 throw new SQLException(ex);
             } catch (ClassNotFoundException ex) {
-                throw new ClassCastException(ex.getMessage());
+                throw new ClassNotFoundException(ex.getMessage());
             }
         }
-       
+
         return cnx;
     }
 
