853fc8f9ee5ecd24e62596a1e3df6cbbe8fdaa57
Antonio Garcia <antonio.garcia.harto@gmail.com>
2023-05-12 13:32:42 +0200

    Cambio Clase Cliente

--- src/models/Cliente.java
+++ src/models/Cliente.java
@@ -4,15 +4,14 @@ import java.util.Arrays;
 
 public class Cliente {
 
-	private int idClientes;
+	private Integer idClientes;
     private String nombre,direccion;
-    private String[] rol= {"Fabricante","Administrador","Cliente"};
+    private String rol;
     private String nombreUsuario,contrasena;
-    
-	public int getIdClientes() {
+	public Integer getIdClientes() {
 		return idClientes;
 	}
-	public void setIdClientes(int idClientes) {
+	public void setIdClientes(Integer idClientes) {
 		this.idClientes = idClientes;
 	}
 	public String getNombre() {
@@ -27,17 +26,17 @@ public class Cliente {
 	public void setDireccion(String direccion) {
 		this.direccion = direccion;
 	}
-	public String[] getRol() {
+	public String getRol() {
 		return rol;
 	}
-	public void setRol(String[] rol) {
+	public void setRol(String rol) {
 		this.rol = rol;
 	}
-	public String getNombreUsario() {
+	public String getNombreUsuario() {
 		return nombreUsuario;
 	}
-	public void setNombreUsario(String nombreUsario) {
-		this.nombreUsuario = nombreUsario;
+	public void setNombreUsuario(String nombreUsuario) {
+		this.nombreUsuario = nombreUsuario;
 	}
 	public String getContrasena() {
 		return contrasena;
@@ -45,7 +44,7 @@ public class Cliente {
 	public void setContrasena(String contrasena) {
 		this.contrasena = contrasena;
 	}
-	public Cliente(int idClientes, String nombre, String direccion, String[] rol, String nombreUsuario,
+	public Cliente(Integer idClientes, String nombre, String direccion, String rol, String nombreUsuario,
 			String contrasena) {
 		super();
 		this.idClientes = idClientes;
@@ -60,9 +59,11 @@ public class Cliente {
 	}
 	@Override
 	public String toString() {
-		return "Cliente [idClientes=" + idClientes + ", nombre=" + nombre + ", direccion=" + direccion + ", rol="
-				+ Arrays.toString(rol) + ", nombreUsario=" + nombreUsuario + ", contrasena=" + contrasena + "]";
+		return "Cliente [idClientes=" + idClientes + ", nombre=" + nombre + ", direccion=" + direccion + ", rol=" + rol
+				+ ", nombreUsuario=" + nombreUsuario + ", contrasena=" + contrasena + "]";
 	}
+    
+	
 
     
 }
