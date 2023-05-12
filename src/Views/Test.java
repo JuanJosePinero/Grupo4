7f9329912c3c083f5fc3849b167d032638dabcd9
SamuraiJuanjo <Juanjosepi.orellana19@gmail.com>
2023-05-12 13:33:53 +0200

    Clase Test ListViewVehiculos

---
+++ src/Views/Test.java
@@ -0,0 +1,16 @@
+package Views;
+
+import javax.swing.JFrame;
+
+public class Test extends JFrame{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ListViewVehiculos listview=new ListViewVehiculos();
+		listview.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		listview.setLocationRelativeTo(null);
+		listview.setVisible(true);
+	}
+
+}
