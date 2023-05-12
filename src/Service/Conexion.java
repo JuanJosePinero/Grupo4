2401cab7cb30e958b77165e5b2c16c3bacb3d3b2
SamuraiJuanjo <Juanjosepi.orellana19@gmail.com>
2023-05-12 13:35:40 +0200

    Mejora clase Conexion

--- src/Service/Conexion.java
+++ src/Service/Conexion.java
@@ -12,7 +12,7 @@ public class Conexion {
         if (cnx == null) {
             try {
                 Class.forName("com.mysql.cj.jdbc.Driver");
-                cnx = DriverManager.getConnection("jdbc:mysql:root@127.0.0.1:3306/gestionra8?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "root");
+                cnx = DriverManager.getConnection("jdbc:mysql://localhost/gestionra8?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "root");
             } catch (SQLException ex) {
                 throw new SQLException(ex);
             } catch (ClassNotFoundException ex) {
