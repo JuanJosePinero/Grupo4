5a3bbc9a2b66e11d382509ff8e48d4d456790abb
HugoSaanchez <hugosaaanchez@gmail.com>
2023-05-12 13:19:29 +0200

    Añadida la clase Conexion y ClientService

---
+++ src/Service/Conexion.java
@@ -0,0 +1,31 @@
+package Service;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+
+    private static Connection cnx = null;
+
+    public static Connection obtener() throws SQLException, ClassNotFoundException {
+        if (cnx == null) {
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                cnx = DriverManager.getConnection("jdbc:mysql:root@127.0.0.1:3306/gestionra8?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "root");
+            } catch (SQLException ex) {
+                throw new SQLException(ex);
+            } catch (ClassNotFoundException ex) {
+                throw new ClassCastException(ex.getMessage());
+            }
+        }
+       
+        return cnx;
+    }
+
+    public static void cerrar() throws SQLException {
+        if (cnx != null) {
+            cnx.close();
+        }
+    }
+}
