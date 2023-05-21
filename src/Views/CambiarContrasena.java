c6f29090abaa5f693b856d9ab836bd2567a04f43
HugoSaanchez <hugosaaanchez@gmail.com>
2023-05-21 02:15:56 +0200

    Cambiada la clase cambiarcontraseña

--- src/Views/CambiarContrasena.java
+++ src/Views/CambiarContrasena.java
@@ -9,6 +9,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
@@ -19,13 +20,13 @@ import models.Cliente;
 public class CambiarContrasena extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField ContT;
-	private JTextField textField_1;
 	private JTextField UsuarioT;
 	private JButton ConfirmarB,CancelarB;
 	private final ClientService services = new ClientService();
 	private Cliente cliente;
 	private int id= ListViewClientes.getidClienteCrear();
+	private JPasswordField ContP;
+	private JPasswordField CContP;
 
 
 
@@ -45,16 +46,6 @@ public class CambiarContrasena extends JFrame {
 		lblNewLabel.setBounds(63, 71, 110, 14);
 		contentPane.add(lblNewLabel);
 		
-		ContT = new JTextField();
-		ContT.setBounds(200, 69, 145, 20);
-		contentPane.add(ContT);
-		ContT.setColumns(10);
-		
-		textField_1 = new JTextField();
-		textField_1.setBounds(200, 99, 145, 20);
-		contentPane.add(textField_1);
-		textField_1.setColumns(10);
-		
 		JLabel lblNewLabel_1 = new JLabel("Confirmar Contraseña");
 		lblNewLabel_1.setBounds(63, 101, 127, 14);
 		contentPane.add(lblNewLabel_1);
@@ -73,6 +64,10 @@ public class CambiarContrasena extends JFrame {
 		contentPane.add(UsuarioT);
 		UsuarioT.setColumns(10);
 		
+		ContP = new JPasswordField();
+		ContP.setBounds(200, 68, 145, 20);
+		contentPane.add(ContP);
+		
 		JLabel lblNewLabel_2 = new JLabel("Usuario");
 		lblNewLabel_2.setBounds(63, 41, 63, 14);
 		contentPane.add(lblNewLabel_2);
@@ -83,9 +78,16 @@ public class CambiarContrasena extends JFrame {
 		ConfirmarB.addActionListener(ma);
 		CancelarB.addActionListener(ma);
 		
+		
 		try {
 			Cliente datos = services.getClienteId(Conexion.obtener(), id);
 			UsuarioT.setText(datos.getNombreUsuario());
+			
+			CContP = new JPasswordField();
+			CContP.setBounds(200, 98, 145, 20);
+			contentPane.add(CContP);
+			
+			
 			System.out.println(datos.getNombreUsuario());
 		} catch (ClassNotFoundException | SQLException e) {
 		}
@@ -102,9 +104,25 @@ public class CambiarContrasena extends JFrame {
 			if(b.equals(ConfirmarB)) {
 				try {
 					Cliente datos = services.getClienteId(Conexion.obtener(), id);
-					String cont = ContT.getText();
+					char[] contC = ContP.getPassword();
+					String cont = new String(contC);
+					char[] ccontC= CContP.getPassword();
+					String ccont = new String(ccontC);
 					datos.setContrasena(cont);
+					
+					if (!cont.matches("[A-Z].*[0-9].*")) {
+						JOptionPane.showMessageDialog(null, "Error");
+					}else if (!cont.equals(ccont))
+						JOptionPane.showMessageDialog(null, "Las contraseñas no coinciden", "Aviso",
+								JOptionPane.ERROR_MESSAGE);
+					else {
 					services.save(Conexion.obtener(), datos);
+					
+					ListViewClientes lvc = new ListViewClientes();
+					
+					dispose();
+					}
+					
 				} catch (ClassNotFoundException | SQLException e1) {
 					System.out.println("selecciona una fila");
 				}
@@ -123,4 +141,5 @@ public class CambiarContrasena extends JFrame {
 		}
 		
 	}
+	
 }
